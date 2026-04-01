8ab993125663f45d4ce15ef98c490b70f08e86ed
Pika <linaomasa929@gmail.com>
2026-04-01 11:51:00 +0800

    1

--- extensions/patch-version-builder/dist/global.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PACKAGE_NAME = void 0;
-exports.PACKAGE_NAME = 'patch-version-builder';
--- extensions/patch-version-builder/source/global.ts
+++
@@ -1,2 +0,0 @@
-
-export const PACKAGE_NAME = 'patch-version-builder';
